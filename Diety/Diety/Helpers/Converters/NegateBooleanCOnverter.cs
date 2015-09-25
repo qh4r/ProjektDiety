2dc9dc2cee4164c1cd720dfbfd22dc574a16e674
Rafał Kucharski <r.kucharski@lgbs.pl>
2015-09-25 21:07:58 +0200

    Implementacja aktywnosci przyciskow w zaleznosci od daty

---
+++ Diety/Diety/Helpers/Converters/NegateBooleanCOnverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Diety.Helpers.Converters
+{
+	public class NegateBooleanConverter : IValueConverter
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Converts a value.
+		/// </summary>
+		/// <param name="value">The value produced by the binding source.</param>
+		/// <param name="targetType">The type of the binding target property.</param>
+		/// <param name="parameter">The converter parameter to use.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		/// <returns>
+		/// A converted value. If the method returns null, the valid null value is used.
+		/// </returns>
+		/// <exception cref="System.NotImplementedException"></exception>
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return !(value as bool?) ?? false;
+		}
+
+		/// <summary>
+		/// Converts a value.
+		/// </summary>
+		/// <param name="value">The value that is produced by the binding target.</param>
+		/// <param name="targetType">The type to convert to.</param>
+		/// <param name="parameter">The converter parameter to use.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		/// <returns>
+		/// A converted value. If the method returns null, the valid null value is used.
+		/// </returns>
+		/// <exception cref="System.NotImplementedException"></exception>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+	}
+}
